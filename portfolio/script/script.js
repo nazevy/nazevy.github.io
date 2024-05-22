8d26cd987f0ab708eb4c5a50b8ede5dbe8435a2e
nazevy <119162590+nazevy@users.noreply.github.com>
2024-05-22 23:10:42 +0530

    Update script.js

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -3,6 +3,23 @@ gsap.registerPlugin(ScrollTrigger)
 const timeline = gsap.timeline({ defaults: { duration: 1 }})
 const myText = new SplitType('.mytext')
 
-timeline
+/*timeline
     .from('.box', { y: '100%', ease: 'bounce' })
-    .to('.char', { y: 0, stagger: 0.05, duration: 0 })
+    .to('.char', { y: 0, stagger: 0.05, duration: 0 })*/
+
+document.querySelectorAll('.box').forEach(box => {
+    timeline
+    .from(box, {
+        y: '100',
+        ease: 'bounce',
+        scrollTrigger: {
+            trigger: box,
+            start: 'top center',
+            end: '+=500',
+            markers: true,
+            scrub: 1,
+            toggleActions: 'play none none reset'
+        }
+    })
+})
+    timeline.to('.char', { y: 0, stagger: 0.05, duration: 0 }) 
