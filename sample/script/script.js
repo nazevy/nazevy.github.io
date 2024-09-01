740ff06dd823991c772d23d2bfd5668b32efd8a9
nazevy <119162590+nazevy@users.noreply.github.com>
2024-09-01 14:40:09 +0530

    Update script.js

--- sample/script/script.js
+++ sample/script/script.js
@@ -1,4 +1,7 @@
-function changeTheme(color) {
+function changeTheme(color) {    
     let cs = color.toLowerCase();
+    document.body.className = '';
+    document.body.classList.add(themeClass);
     document.getElementById("theme").setAttribute('href','../theme/'+cs+'.css');
+    
 }
