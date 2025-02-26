494cc3f28c78e14d221b55bd5646928ac352b40e
nazevy <119162590+nazevy@users.noreply.github.com>
2025-02-26 14:20:14 +0530

    Update script.js

--- sample/script/script.js
+++ sample/script/script.js
@@ -3,5 +3,12 @@ function changeTheme(color) {
     document.body.className = '';
     document.body.classList.add(cs);
     document.getElementById("theme").setAttribute('href','../theme/'+cs+'.css');
-    
 }
+
+const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navigation');
+
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('responsive'); // Toggle the responsive class
+    console.log('Hamburger clicked!'); // Debugging: Check if the click event is firing
+});
